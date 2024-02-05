4b89bb7871967055033abef52feec71458dc748b
Gesugao-san <gesugao.san@gmail.com>
2024-02-05 04:18:57 +0300

    Tune CSS

--- src/App.vue
+++ src/App.vue
@@ -41,12 +41,9 @@ header {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    /* padding-right: calc(var(--section-gap) / 2); */
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
   header .wrapper {
     display: flex;
