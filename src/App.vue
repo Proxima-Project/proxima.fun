2534847d934709c9985baf6cf946c772ed215ce2
Gesugao-san <gesugao.san@gmail.com>
2024-02-08 21:10:07 +0300

    Add much more icons, normalizing css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 
-<script setup lang="ts">
+<script setup lang='ts'>
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faGithub,
@@ -7,13 +7,15 @@ import {
   faDiscord
 } from '@fortawesome/free-brands-svg-icons'
 
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import ProjectInfo from './components/ProjectInfo.vue'
+
 import OpenDiscord from './components/Open/OpenDiscord.vue'
 import OpenGitHub from './components/Open/OpenGitHub.vue'
 import OpenWiki from './components/Open/OpenWiki.vue'
-import ProjectInfo from './components/ProjectInfo.vue'
-import SocialMediaLinks from "./components/SocialMediaLinks.vue"
+
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import SocialMediaLinks from './components/SocialMediaLinks.vue'
 
 library.add(
   faGithub,
@@ -31,17 +33,24 @@ library.add(
       <source alt='Vue logo' class='logo' src='./assets/proxima5.webm' type='video/webm' />
     </video> -->
 
-    <!-- <div class="circular" v-bind:style="{ 'background-image': 'url(' + './assets/proxima5.webm' + ')' }"></div> -->
+    <!-- <div class='circular' v-bind:style='{ 'background-image': 'url(' + './assets/proxima5.webm' + ')' }'></div> -->
 
-    <div class="container wrapper">
-      <video autoplay muted loop width='467' height='467' poster='@/assets/proxima4.png'>
-        <source alt='Vue logo' class='logo' src='./assets/proxima5.webm' type='video/webm' />
+    <div class='container wrapper'>
+      <!-- <video autoplay muted loop width='467' height='467' poster='./assets/SS13/Proxima/proxima7-BoS.png' id='bgvideo'>
+        <source alt='Vue logo' class='logo' src='./assets/SS13/Proxima/proxima8-BoS.webm' type='video/webm' />
+      </video> -->
+
+      <video playsinline autoplay muted loop width='467' height='467' poster='./assets/SS13/Proxima/proxima7-BoS.png' >
+        <source alt='Vue logo' class='logo' src='./assets/SS13/Proxima/proxima8-BoS.webm' type='video/webm' />
       </video>
-      <video playsinline autoplay muted loop width='467' height='467' poster='@/assets/proxima4.png' id="bgvideo" >
-        <source src='@/assets/proxima5.webm' type='video/webm'>
-      </video>
-      <HelloWorld class="centered" :msg='`${ProjectInfo.name}`' />
+
+      <!-- <div class="greetings">
+        <h1 class="green"><a href="https://en.wikipedia.org/wiki/Proxima_Centauri" target="_blank" rel="noopener">{{ ProjectInfo.name }}</a></h1>
+      </div> -->
+
+      <HelloWorld class='centered' :msg='`${ProjectInfo.name}`' />
     </div>
+
   </header>
 
   <main>
@@ -52,15 +61,23 @@ library.add(
       https://fontawesome.com/v6/docs/web/style/animate
     -->
 
-    <!-- <font-awesome-icon class="icon alt" fixed-width font-size=8em :icon="['fab', 'discord']" beat /> -->
-    <!-- <div class="icon alt" fixed-width style="font-size: 8em; --fa-animation-duration: 5s; --fa-beat-scale: 0.9; margin-left: 10px;" > -->
-    <div class="icon alt" fixed-width style="font-size: 8em; margin-left: 10px;" >
+    <!-- <font-awesome-icon class='icon alt' fixed-width font-size=8em :icon='['fab', 'discord']' beat /> -->
+    <!-- <div class='icon alt' fixed-width style='font-size: 8em; --fa-animation-duration: 5s; --fa-beat-scale: 0.9; margin-left: 10px;' > -->
+    <div class='icon alt' fixed-width >
       <OpenDiscord />
       <OpenGitHub />
       <OpenWiki />
     </div>
-    <!-- <SocialMediaLinks title="@hrwebdevelopers" description="We build blazing-fast, beautiful websites for startups, small businesses and enterprises." /> -->
+    <!-- <SocialMediaLinks title='@hrwebdevelopers' description='We build blazing-fast, beautiful websites for startups, small businesses and enterprises.' /> -->
   </main>
+
+  <footer>
+    <h4>
+      powered by
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> &
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>
+    </h4>
+  </footer>
 </template>
 
 <style scoped>
@@ -77,6 +94,15 @@ library.add(
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+h1, div { text-shadow:
+  black 0px 1px 20px, black 0px 1px 20px,
+  black 0px 1px 20px, black 0px 1px 20px;
+}
+
+footer {
+  position:fixed;bottom:0;left:0;right:0;height:30px;
 }
 
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,11 +10,6 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green"><a href="https://en.wikipedia.org/wiki/Proxima_Centauri" target="_blank" rel="noopener">{{ msg }}</a></h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
   </div>
 </template>
 
